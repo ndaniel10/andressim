9cc0e73bb5c3842ce3620b49fa2f1884fddbfbe3
ndaniel10 <noahdaniel10@gmail.com>
2018-12-08 23:57:31 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,77 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.DISCORD_BOT_SECRET;
+const { Client, Attachment } = require('discord.js');
+
+
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Minecraft Porn™`);
+});
+
+//general and messages
+
+client.on('message', (message) =>{
+    if(message.content === ':a') {
+        message.channel.send('shut up you`re a gay faggot :middle_finger:')
+    
+        message.delete(message)
+    } 
+    
+});
+
+client.on('message', (message) =>{
+    if(message.content === ':b') {
+        message.channel.send('wa')
+    
+        message.delete(message)
+    } 
+    
+});
+
+client.on('message', (message) =>{
+  if (message.content === ':wa') {
+    message.channel.send('wa')
+    message.channel.send('waa')
+    message.channel.send('waaa')
+    message.channel.send('waaaa')
+
+
+  }
+});
+
+client.on('message', (message) => {
+    if(message.content === ':rank') {
+        const attachment = new Attachment('https://cdn.discordapp.com/attachments/489843725007192074/517043007690899485/unknown.png');
+        message.channel.send(attachment);
+    }
+});
+
+  
+  client.on('message', (message) =>{
+    if (message.content === 'Sergio Ramos')  {
+        const attachment = new Attachment('http://i37.tinypic.com/ok0pqd.jpg');
+        message.channel.send(attachment);
+    }
+});
+  
+
+
+client.on('message', (message) =>{
+    if(message.content.startsWith(':example')) {
+        const attachment = new Attachment('https://cdn.discordapp.com/attachments/489843725007192074/514894613157511168/unknown.png');
+        message.channel.send(attachment);
+    }
+});
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(ch => ch.name === 'welcome👋');
+  if (!channel) return;
+  channel.send(`Go to ▶verification◀ for the next step ${member}`);
+});
+
+client.login(token);
+
